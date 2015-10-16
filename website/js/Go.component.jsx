1d8fd44d276b4d242d638a32c96404d562eb56d2
Ed Henderson <ed@teamhendo.com>
2015-10-15 18:05:20 -0700

    Weeeeeeee

--- website/js/Go.component.jsx
+++ website/js/Go.component.jsx
@@ -4,11 +4,13 @@ var Go = React.createClass({
     render: function() {
     	if (this.props.gender !== '' && this.props.thumbnail !== '') {
 	        return (
-	            <input
-	                className="primary"
-	                type="submit"
-	                value="Match Me"
-	            />
+	        	<div className="control -primary">
+		            <input
+		                className="primary"
+		                type="submit"
+		                value="Match Me"
+		            />
+		        </div>
 	        );
     	}
     	else {
